395fda3da543b97bc2fd208b50744fa621bbed99
Dinesh <dhakalbhai24@gmail.com>
2024-06-20 23:38:37 -0700

    Spring Boot project for Car, initial commit

---
+++ carproject/src/main/java/com/spring/carproject/CarProject.java
@@ -0,0 +1,22 @@
+package com.spring.carproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@SpringBootApplication
+@RestController
+public class CarProject {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarProject.class, args);
+	}
+	@GetMapping
+	public List<String> showMessage(){
+		return List.of("Red Car", "Blue Car");
+	}
+
+}
---
+++ carproject/src/test/java/com/spring/carproject/CarprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.carproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
